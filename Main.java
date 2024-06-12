699aebeab0ef0fe83267e5d51e87e94fb5e8d252
ahammed-nibras <ahammednibras@proton.me>
2024-06-12 09:51:56 +0530

    Inheritance And Polymorphism

---
+++ Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        Animal Donkey = new Animal();
+
+        Donkey.sound();
+
+        Donkey = new Donkey();
+        
+        Donkey.sound();
+    }
+}
+
+class Animal {
+    void sound() {
+        System.out.println("Animal makes sound");
+    }
+}
+
+class Donkey extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Donkey makes sounds");
+    }
+}
